7c0a1e74f01935409460ad19926bd4e65775254f
Corentin <corentinys@gmail.com>
2026-04-17 12:02:48 +0200

    mise en place de react

---
+++ quiz-app/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+
+
+function App() {
+
+
+  return (
+    <>
+        <h1>Welcom to the frontend quizz</h1>
+        <div>
+            <ul>
+                <li>
+                    <a href="">HTML</a>
+                </li>
+                <li>
+                    <a href="">CSS</a>
+                </li>
+                <li>
+                    <a href="">Javascript</a>
+                </li>
+                <li>
+                    <a href="">Accessibility</a>
+                </li>
+            </ul>
+        </div>
+    </>
+  )
+}
+
+export default App
